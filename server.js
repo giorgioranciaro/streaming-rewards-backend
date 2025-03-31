edf8c8c18d0a26009e39e008d1cd15b43b7d3c09
Giorgio Ranciaro <srs@Giorgios-MBP-2.fritz.box>
2025-03-31 20:20:19 +0200

    Fix: bind server to 0.0.0.0 for Railway

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get("/", (req, res) => {
   res.send("🎧 Streaming Rewards Backend API");
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
